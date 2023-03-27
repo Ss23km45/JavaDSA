20bdb2f3b853c38d7434ef04c9147a1172a1957c
Mukesh_DevopsENG <surya1999challa@gmail.com>
2023-03-27 20:27:47 +0530

    #Cyclic Sorting and Repeated and all missed numbers

--- src/main/java/util/FindDuplicates.java
+++ src/main/java/util/FindDuplicates.java
@@ -2,33 +2,36 @@ package util;
 
 import FindSwap.Swapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class RepeatedElement {
+public class FindDuplicates {
     static Swapping swapping = new Swapping();
     public static void main(String[] args) {
-        int[] arr = {5,2,7,4,8,8,1,6};
+        int[] arr = {5,2,7,4,8,3,3,6};
         System.out.println("Repeated Number = " + missedNumber(arr));
     }
 
-    public static int missedNumber(int[] arr){
+    public static List<Integer> missedNumber(int[] arr){
         for (int i = 0; i < arr.length ; i++) {
-            int correct = arr[i] - 1;
-            if(arr[i] == arr[correct]){
-                continue;
+            int correct = arr[i];
+            if(arr[i] < arr.length && arr[i] != arr[correct]) {
+                swapping.swap(arr,i,correct);
             }
             else{
-                swapping.swap(arr,i,correct);
+                continue;
             }
         }
         System.out.println("arr = " + Arrays.toString(arr));
+        List<Integer> dup = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] != i+1 ){
-                return arr[i];
+                dup.add(i+1);
             }
         }
 
 
-        return arr.length;
+        return dup;
     }
 }
