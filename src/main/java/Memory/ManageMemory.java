b80f963e42a9a268dd0f628e29c7a90909a196b8
Mukesh_DevopsENG <surya1999challa@gmail.com>
2023-03-29 20:46:23 +0530

    #Cyclic Sorting and Repeated and all missed numbers

---
+++ src/main/java/Memory/ManageMemory.java
@@ -0,0 +1,14 @@
+package Memory;
+
+import java.util.Arrays;
+
+public class ManageMemory {
+    public static void main(String[] args) {
+        int[] a = {1,4,2,5,34,46,6,3,3,245,2434};
+        int[] b =a;
+        System.out.println(Arrays.toString(a));
+        String ac;
+        System.out.println(Arrays.toString(b));
+
+    }
+}
---
+++ src/main/java/util/Palindrome.java
@@ -0,0 +1,25 @@
+package util;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String pali = "gthhtg";
+        int start=0, end =pali.length()-1;
+        int mid = end/2;
+
+        while (start<= end){
+
+            if(pali.charAt(start) == pali.charAt(end)){
+                start++;
+                end--;
+            }
+            else{
+                System.out.println("Not a Palindrome");
+                break;
+            }
+
+        }
+        if((start >= mid) && (end <= mid)){
+            System.out.println("Given String is a Palindrome"  + " Start " + start + " End " + end + " Mid is " + mid);
+        }
+    }
+}
